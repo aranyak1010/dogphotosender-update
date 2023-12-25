5d95d8d7ce42fda728f1c17c495c033010a9e283
Amartyanil Chattopadhyay <61569496+amartyanil@users.noreply.github.com>
2023-12-25 13:57:02 +0530

    Allowed the user to send at difference frequencies

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import ezgmail
+import time
 
 dog_image_url = requests.get("https://dog.ceo/api/breeds/image/random").json()["message"]
 dog_image_url_response = requests.get(dog_image_url)
@@ -12,11 +13,34 @@ email_id_list = []
 no_of_email_ids = input("Enter number of email ids: ")
 no_of_email_ids = int(no_of_email_ids)
 
+print("Please input the e-mail ids one by one: ")
 for i in range(0,no_of_email_ids):
     email_id = input()
     email_id_list.append(email_id)
 
-subject = "Daily Dog Picture"
-text = "This is a dog picture."
-for i in range(0,no_of_email_ids):
-    ezgmail.send(email_id_list[i], subject, text, "dog_image.png")
+while True:
+    print("Enter frequency, 1. just once 2. every hour 3. daily")
+    frequency = input()
+    if frequency == "1": 
+        subject = "Dog Picture"
+        text = "This is a dog picture."
+        for i in range(0,no_of_email_ids):
+            ezgmail.send(email_id_list[i], subject, text, "dog_image.png")
+        break
+    elif frequency == "2":
+        subject = "Dog Picture"
+        text = "This is a dog picture."
+        while True:
+            if time.ctime().split()[3].split(':')[1] == "00" and time.ctime().split()[3].split(':')[2] == "00":
+                for i in range(0,no_of_email_ids):
+                    ezgmail.send(email_id_list[i], subject, text, "dog_image.png")
+    elif frequency == "3":    
+        subject = "Dog Picture"
+        text = "This is a dog picture."
+        while True:
+            if time.ctime().split()[3] == "00:00:00":
+                for i in range(0,no_of_email_ids):
+                    ezgmail.send(email_id_list[i], subject, text, "dog_image.png") 
+    else:
+        print("Please try again")
+        continue
